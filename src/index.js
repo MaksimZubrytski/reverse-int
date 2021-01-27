d34a98fb32b0b7d1d0cb0346697718611986b0fc
MaksimZubrytski <69052702+MaksimZubrytski@users.noreply.github.com>
2021-01-28 00:06:35 +0300

    feat: add solve task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function reverse (n) {
-  
+    if(n < 0) {
+        let newNumber = -n
+        let newString = String(newNumber)
+        let reverseString = newString.split('').reverse().join('');
+        let reverseNumber = Number(reverseString)
+        return reverseNumber;
+    }
+    else {
+        let newString = String(n)
+        let reverseString = newString.split('').reverse().join('');
+        let reverseNumber = Number(reverseString)
+        return reverseNumber;
+    }
 }
